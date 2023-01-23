98e238e3b04460988ec673d62a50682b0d1e76fc
cohet3 <tauro.total90@gmail.com>
2023-01-23 21:26:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const express= require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { expressjwt: expressJwt } = require('express-jwt')
+const User = require('./user')
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://danny:EaZanGMNpighMYsB@menu-db.wdyhe.mongodb.net/auth?retryWrites=true&w=majority')
+const app = express()
+
+app.use(express.json())
+
+console.log(process.env.SECRET)
+
+const validateJwt = expressJwt({ secret: process.env.SECRET, algorithms:['HS256']})
+
+const signToken = _id =>jwt.sign({ _id }, process.env.SECRET)
+app.post('/register', async(req,res)=>{
+    const { body } = req
+    console.log({ body })
+    try{
+        const isUser = await User.findOne({ email:body.email })
+        if (isUser){
+            return res.status(403).send('usuario ya existe')
+        }
+        const salt = await bcrypt.genSalt()
+        const hashed = await bcrypt.hash(body.password, salt)
+        const user = await User.create({ email: body.email, password: hashed , salt})
+       const signed = signToken(user._id)
+        res.status(201).send(signed)
+
+    }catch (err){
+        console.log(err)
+        res.status(500).send(err.message)
+    }
+})
+app.post('/login', async (req, res) => {
+    const { body }= req
+    try{
+        const user = await User.findOne({ email: body.email })
+        if(!user) {
+            res.status(403).send('usuario o contraseña inválida')
+    
+        }else {
+            const isMatch = await bcrypt.compare(body.password, user.password)
+            if (isMatch) { 
+                const signed = signToken(user._id)
+                res.status(200).send(signed)
+            }else{
+                res.status(403).send('usuario y/o contraseña inválida')
+            }
+        }
+
+    }catch(err) {
+        res.status(500).send(err.message)
+    }
+})
+const findAndAssignUser =async (req, res, next) => {
+    try{
+     const user = await User.findById(req.auth._id)
+     if (!user) {
+         return res.status(401).end()
+     }
+     req.user = user
+     next()
+   } catch (e){
+     next(e)
+   }
+ }
+ const isAuthenticated = express.Router().use(validateJwt, findAndAssignUser)
+
+app.get('/lele', isAuthenticated, (req, res) => {
+    throw new Error ('menudo error, corre')
+    res.send(req.auth)
+})
+//midelware para errores imprimidos en consola
+app.use((err, req, res, next) => {
+    console.error('Mi nuevo error', err.stack)
+    next(err)
+})
+app.use((err, req, res, next) => {
+    res.send('Ha ocurrido un error :(')
+})
+ app.listen(3000, () =>{
+    console.log('listenig in port 3000')
+ })
